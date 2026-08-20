5ab0bd736e24bd306f05cfa9895dc15c30cfd4f1
Alesia <alesya.17039722@yandex.ru>
2026-08-20 13:28:30 +0300

    Fix food spawn validation

--- the_snake.py
+++ the_snake.py
@@ -1,12 +1,16 @@
 from random import choice, randint
 
-import pygame
+import pygame as pg
 
 # Константы для размеров поля и сетки:
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
+CENTER_POSITION = (
+    (GRID_WIDTH // 2 - 1) * GRID_SIZE,
+    (GRID_HEIGHT // 2 - 1) * GRID_SIZE,
+)
 
 # Направления движения:
 UP = (0, -1)
@@ -48,83 +52,84 @@ SPEED_UP_TIME = 15000
 SPEED = 5
 
 # Настройка игрового окна:
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-
-# Заголовок окна игрового поля:
-pygame.display.set_caption('Змейка')
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 # Настройка времени:
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 
 class GameObject:
     """Базовый класс для игровых объектов."""
 
     def __init__(self, body_color=None):
-        self.position = (
-            (GRID_WIDTH // 2 - 1) * GRID_SIZE,
-            (GRID_HEIGHT // 2 - 1) * GRID_SIZE,
-        )
+        self.position = CENTER_POSITION
         self.body_color = body_color
 
     def draw(self):
         """Метод отвечает за отрисовку объектов."""
-        pass
 
 
 class Food(GameObject):
     """Базовый класс для игорвых объектов еды."""
 
-    def __init__(self, body_color):
+    def __init__(self, body_color, snake_positions=None):
         super().__init__(body_color)
-        self.randomize_position()
+        self.randomize_position(snake_positions)
 
-    def randomize_position(self):
+    def randomize_position(self, snake_positions=None):
         """Устанавливает случайную позицию объекта на игровом поле."""
-        self.position = (
-            randint(0, GRID_WIDTH - 1) * GRID_SIZE,
-            randint(0, GRID_HEIGHT - 1) * GRID_SIZE,
-        )
+        if snake_positions is None:
+            snake_positions = []
+
+        while True:
+            position = (
+                randint(0, GRID_WIDTH - 1) * GRID_SIZE,
+                randint(0, GRID_HEIGHT - 1) * GRID_SIZE,
+            )
+
+            if position not in snake_positions:
+                self.position = position
+                break
 
     def draw(self):
         """Метод отвечает за отрисовку объектов еды."""
-        rect = pygame.Rect(
+        rect = pg.Rect(
             (self.position[0] + CELL_PADDING, self.position[1] + CELL_PADDING),
             (GRID_SIZE - CELL_PADDING * 2, GRID_SIZE - CELL_PADDING * 2),
         )
-        pygame.draw.rect(screen, self.body_color, rect)
-        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+        pg.draw.rect(screen, self.body_color, rect)
+        pg.draw.rect(screen, BORDER_COLOR, rect, 1)
 
 
 class Apple(Food):
-    """Класс описания объекта яблока"""
+    """Класс описания объекта яблока."""
 
-    def __init__(self):
-        super().__init__(APPLE_COLOR)
+    def __init__(self, snake_positions=None):
+        super().__init__(APPLE_COLOR, snake_positions)
 
 
 class PoisonousFood(Food):
-    """Ядовитая еда для уменьшения длины змейки"""
+    """Ядовитая еда для уменьшения длины змейки."""
 
-    def __init__(self):
-        super().__init__(POISONOUS_FOOD_COLOR)
-        self.spawn_time = pygame.time.get_ticks()
+    def __init__(self, snake_positions):
+        super().__init__(POISONOUS_FOOD_COLOR, snake_positions)
+        self.spawn_time = pg.time.get_ticks()
         self.active = True
         self.disappear_time = None
 
-    def disappear_food(self):
+    def disappear_food(self, snake_positions):
         """Управляет исчезновением и появлением ядовитой еды."""
-        current_time = pygame.time.get_ticks()
+        current_time = pg.time.get_ticks()
 
         if self.active:
             if current_time - self.spawn_time > LIFETIME:
                 self.active = False
-                self.disappear_time = pygame.time.get_ticks()
+                self.disappear_time = pg.time.get_ticks()
         else:
             if current_time - self.disappear_time > RESPAWN_FOOD_TIME:
                 self.active = True
-                self.spawn_time = pygame.time.get_ticks()
-                self.randomize_position()
+                self.spawn_time = pg.time.get_ticks()
+                self.randomize_position(snake_positions)
 
 
 class Snake(GameObject):
@@ -132,11 +137,7 @@ class Snake(GameObject):
 
     def __init__(self):
         super().__init__(SNAKE_COLOR)
-        self.length = 1
-        self.positions = [self.position]
-        self.direction = RIGHT
-        self.next_direction = None
-        self.last = None
+        self.reset(RIGHT)
 
     def get_head_position(self):
         """Возвращает позицию головы змейки."""
@@ -144,17 +145,16 @@ class Snake(GameObject):
 
     def move(self):
         """Осуществляет движение змейки."""
-        x, y = self.get_head_position()
-        dx, dy = self.direction
+        head_x, head_y = self.get_head_position()
+        direction_x, direction_y = self.direction
         new_position = (
-            (x + dx * GRID_SIZE) % SCREEN_WIDTH,
-            (y + dy * GRID_SIZE) % SCREEN_HEIGHT,
+            (head_x + direction_x * GRID_SIZE) % SCREEN_WIDTH,
+            (head_y + direction_y * GRID_SIZE) % SCREEN_HEIGHT,
         )
         self.positions.insert(0, new_position)
 
         while len(self.positions) > self.length:
             self.last = self.positions.pop()
-            screen.fill(BOARD_BACKGROUND_COLOR)
 
     def update_direction(self):
         """Метод обновления направления после нажатия на кнопку."""
@@ -165,23 +165,24 @@ class Snake(GameObject):
     def draw(self):
         """Метод отвечает за отрисовку змейки."""
         for position in self.positions:
-            rect = pygame.Rect(position, (GRID_SIZE, GRID_SIZE))
-            pygame.draw.rect(screen, self.body_color, rect)
-            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+            rect = pg.Rect(position, (GRID_SIZE, GRID_SIZE))
+            pg.draw.rect(screen, self.body_color, rect)
+            pg.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-    def reset(self):
+    def reset(self, direction=None):
         """Метод сбрасывает атрибуты змейки до начального."""
         self.length = 1
         self.positions = [self.position]
-        self.direction = choice((UP, DOWN, RIGHT, LEFT))
+        self.direction = direction or choice((UP, DOWN, RIGHT, LEFT))
         self.next_direction = None
         self.last = None
+        self.score = 1
 
 
 def draw_board():
     """Метод отвечает за отрисовку сетки."""
     for i in range(0, SCREEN_HEIGHT, GRID_SIZE):
-        pygame.draw.line(
+        pg.draw.line(
             screen,
             GRID_COLOR,
             (0, i + GRID_SIZE),
@@ -189,7 +190,7 @@ def draw_board():
         )
 
     for j in range(0, SCREEN_WIDTH, GRID_SIZE):
-        pygame.draw.line(
+        pg.draw.line(
             screen,
             GRID_COLOR,
             (j + GRID_SIZE, 0),
@@ -199,58 +200,59 @@ def draw_board():
 
 def handle_keys(game_object):
     """Функция обработки действий пользователя."""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             raise SystemExit
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and game_object.direction != DOWN:
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_UP and game_object.direction != DOWN:
                 game_object.next_direction = UP
-            elif event.key == pygame.K_DOWN and game_object.direction != UP:
+            elif event.key == pg.K_DOWN and game_object.direction != UP:
                 game_object.next_direction = DOWN
-            elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+            elif event.key == pg.K_LEFT and game_object.direction != RIGHT:
                 game_object.next_direction = LEFT
-            elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+            elif event.key == pg.K_RIGHT and game_object.direction != LEFT:
                 game_object.next_direction = RIGHT
+            elif event.key == pg.K_ESCAPE:
+                pg.quit()
+                raise SystemExit
 
 
 def main():
     """Запускает игру и управляет основным игровым циклом."""
-    pygame.init()
+    pg.init()
 
     # Время последнего увеличения скорости
-    last_time = pygame.time.get_ticks()
+    last_time = pg.time.get_ticks()
 
     speed = SPEED
 
-    apple = Apple()
     snake = Snake()
-    poison = PoisonousFood()
+    apple = Apple(snake.positions)
+    poison = PoisonousFood(snake.positions)
 
-    running = True
-
-    while running:
+    while True:
 
         handle_keys(snake)
         snake.update_direction()
         snake.move()
-        poison.disappear_food()
+        poison.disappear_food(snake.positions)
 
         if snake.get_head_position() == apple.position:
             snake.length += 1
-            apple.randomize_position()
-        elif snake.get_head_position() in snake.positions[1:]:
-            snake.reset()
-            screen.fill(BOARD_BACKGROUND_COLOR)
+            snake.score += 1
+            apple.randomize_position(snake.positions)
+        elif snake.get_head_position() in snake.positions[4:]:
+            speed, last_time = reset_game(snake, apple, poison)
         elif snake.get_head_position() == poison.position:
             if poison.active:
                 if snake.length == 1:
-                    snake.reset()
-                    screen.fill(BOARD_BACKGROUND_COLOR)
+                    speed, last_time = reset_game(snake, apple, poison)
                 else:
                     snake.length -= 1
-                poison.randomize_position()
-                poison.spawn_time = pygame.time.get_ticks()
+                    snake.score -= 1
+                    poison.randomize_position(snake.positions)
+                    poison.spawn_time = pg.time.get_ticks()
 
         screen.fill(BOARD_BACKGROUND_COLOR)
 
@@ -261,21 +263,37 @@ def main():
         if poison.active:
             poison.draw()
 
-        pygame.display.update()
+        # Заголовок окна игрового поля:
+        pg.display.set_caption(f'Змейка | Cчет: {snake.score} | ESC - выход')
+
+        pg.display.update()
 
         speed, last_time = update_speed(speed, last_time)
         clock.tick(speed)
 
-    pygame.quit()
-
 
 def update_speed(speed, last_time):
     """Увеличивает скорость игры через заданный интервал времени."""
-    current_time = pygame.time.get_ticks()
+    current_time = pg.time.get_ticks()
 
     if current_time - last_time > SPEED_UP_TIME:
         speed += 2
         last_time = current_time
+
+    return speed, last_time
+
+
+def reset_game(snake, apple, poison):
+    """Сбрасывает игру до первоначального состояния."""
+    snake.reset()
+    apple.randomize_position(snake.positions)
+    poison.randomize_position(snake.positions)
+    poison.active = True
+    poison.spawn_time = pg.time.get_ticks()
+    poison.disappear_time = None
+
+    speed = SPEED
+    last_time = pg.time.get_ticks()
 
     return speed, last_time
 
